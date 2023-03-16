fed1c59a0777e67bad0d4b5e5d1f593fe433a08b
mohammedkrit <hj72475@gmail.com>
2023-03-16 16:07:52 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*that is commen */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }	
