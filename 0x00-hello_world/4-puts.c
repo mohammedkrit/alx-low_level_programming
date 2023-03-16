f4635770eec0c5d29921460d920c5a8d8bdb98b4
mohammedkrit <hj72475@gmail.com>
2023-03-16 16:02:31 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 /*that is commen */
-int main(void) {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+int main(void)
+{
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }	
