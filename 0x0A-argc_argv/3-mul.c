929c153e66ba0b0a59bd02a1f402e0ba359f4713
mohammedkrit <mohamedkrit20@gmail.com>
2023-04-10 17:20:14 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,20 +6,18 @@
  * @argv: ls
  * Return: 0 if she  failed 1 if she is success
  */
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	int n1 = atoi(argv[1]);
-	int n2 = atoi(argv[2]);
-	int result = n1 * n2;
+	int result;
 
-	if (argc == 3)
-	{
-		printf("%d\n", result);
-		return (0);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	result = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d\n", result);
+
+	return( 0);
 }
