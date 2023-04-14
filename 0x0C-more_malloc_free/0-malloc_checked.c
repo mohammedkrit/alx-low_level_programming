7183af80aca7802e163feede8d7a602174c897bb
mohammedkrit <mohamedkrit20@gmail.com>
2023-04-14 13:03:50 -0700

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit (98);
+	}
+	return (ptr);
+}
