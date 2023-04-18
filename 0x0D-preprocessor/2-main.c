2e9cbad1d80cbcb13e92cf548239a46491da2f6a
mohammedkrit <mohamedkrit20@gmail.com>
2023-04-17 17:53:56 -0700

    0

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function tatprint comp
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int n;
+
+	__FILE__  =  n;
+
+	printf("the name of the comp is : %s\n", n);
+	return (0);
+}
