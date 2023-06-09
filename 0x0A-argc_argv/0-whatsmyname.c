55cc6698227dd7ea9049967ad8536e26e1469a4a
mohammedkrit <mohamedkrit20@gmail.com>
2023-06-09 09:03:15 -0700

    00

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  * return: 0
  */
 
-int main(int argc,char *argv)
+int main(int argc,char const *argv)
 {
-	argc(void);
-	printf ("%s\n",argv[0]);
+	(void) argc;
+	printf ("%s\n", argv[0]);
 	return (0);
 }
