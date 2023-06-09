1111aa878df8777607e19dd5088957b14a81fb06
mohammedkrit <mohamedkrit20@gmail.com>
2023-06-09 04:52:56 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function that print name of program
+ * @argc: int
+ * @argv: list
+ * return: 0
+ */
+
+int main(int argc,char argv[0])
+{
+	printf ("%s\n",argv[0]);
+	return (0);
+}
