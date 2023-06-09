7350ac061e3e05782203e22e6617f9a98fa2d737
mohammedkrit <mohamedkrit20@gmail.com>
2023-06-09 15:08:46 -0700

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - function that prints all arguments it receives.
+ * @argc: int
+ * @argv: ls
+ * Return: 0
+ */
+
+int main(int argc, char const *argv)
+{
+	for (int i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
