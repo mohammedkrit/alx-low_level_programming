92190014f35d3b8e44fb4a5f4d86b107fb9e7eda
mohammedkrit <mohamedkrit20@gmail.com>
2023-06-11 15:20:25 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ */
+
+int main(int argc, char const *argv)
+{
+	if(argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+ for (i = 0; i < argc; i++)
+        {
