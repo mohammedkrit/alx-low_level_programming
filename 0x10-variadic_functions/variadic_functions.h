2597864d4bab22f097e7ac73416f585b48c756da
mohammedkrit <mohamedkrit20@gmail.com>
2023-06-23 16:16:23 +0100

    0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,10 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int sum_them_all(const unsigned int n, ...);
 
 #endif /*VARIADIC_FUNCTION_H*/
